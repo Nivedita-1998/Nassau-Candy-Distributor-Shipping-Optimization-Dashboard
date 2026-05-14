48f6c7cbd78d81037b6dd108f0fee480d500898e
Nivedita-1998 <niveditamathur1998@gmail.com>
2026-05-14 22:27:55 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -286,11 +286,17 @@ st.sidebar.caption("Use these filters to explore shipment performance by date, r
 min_date = df["Order Date"].min().date()
 max_date = df["Order Date"].max().date()
 
-
-date_range = st.sidebar.date_input(
-    "Select Date Range",
-    [df['Order Date'].min(), df['Order Date'].max()]
+ate_range = st.sidebar.date_input(
+    "📅 Order Date Range",
+    value=(min_date, max_date),
+    min_value=min_date,
+    max_value=max_date
 )
+
+if isinstance(date_range, tuple) and len(date_range) == 2:
+    start_date, end_date = date_range
+else:
+    start_date, end_date = min_date, max_date
 
 
 ship_modes = st.sidebar.multiselect(
